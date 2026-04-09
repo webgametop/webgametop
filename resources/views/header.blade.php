b9f915af42ab14fc2211cbc0c40be90568ba7f8d
Noilty <thenoilty@gmail.com>
2026-04-10 00:31:24 +0300

    fixed tmpl

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,39 +1,27 @@
-<header class="navbar navbar-expand-md py-2">
-    <div class="container justify-content-start align-items-center">
+<header class="navbar navbar-expand-md py-2 d-print-none">
+    <div class="container">
+        {{-- BEGIN NAVBAR TOGGLER --}}
         <button
-            class="navbar-toggler mx-1 me-3 collapsed"
-            style="font-size: 25px;"
+            class="navbar-toggler"
+            style="font-size: 16px;"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbar-menu"
+            aria-controls="navbar-menu"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
         >
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="h1 navbar-brand navbar-brand-autodark d-none-navbar-horizontal me-auto p-0 pe-3">
-            <a href="/" class="text-decoration-none text-uppercase" title="Главная">
+        {{-- END NAVBAR TOGGLER --}}
+        {{-- BEGIN NAVBAR LOGO --}}
+        <div class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
+            <a href="/" class="h1 m-0 text-decoration-none text-uppercase" title="Главная">
                 {{ config('app.name') }}
             </a>
         </div>
-        @if($boosty_url = config('boosty.url'))
-            <div class="d-none d-md-block pe-2">
-                <a
-                    href="{{ $boosty_url }}"
-                    class="btn py-2"
-                    target="_blank"
-                    rel="noreferrer"
-                    title="Внести свой вклад в развитие проекта"
-                >
-                    <img
-                        style="margin-right: 10px;"
-                        src="{{ asset('static/media/boosty.png') }}"
-                        width="18"
-                        alt="Boosty"
-                    >
-                    <span class="text-uppercase">Стать спонсором</span>
-                </a>
-            </div>
-        @endif
-        <div class="flex-grow-1 d-none d-md-block pe-3">
+        {{-- TELEGRAM --}}
+        <div class="d-none d-lg-block pe-3">
             <a
                 href="https://t.me/webgametop"
                 class="btn p-0"
@@ -52,6 +40,7 @@
                 >
             </a>
         </div>
+        {{-- END NAVBAR LOGO --}}
         <div class="navbar-nav flex-row order-md-last align-items-center">
             @guest
                 <a
@@ -66,7 +55,7 @@
                 </a>
             @else
                 @php($user = auth()->user())
-                <div class="nav-item me-3 d-none d-md-flex">
+                <div class="nav-item me-3">
                     <div class="btn-list">
                         @foreach([
                             ['label' => 'Мои уведомления', 'icon' => 'fa fa-bell'],
@@ -87,16 +76,16 @@
                         @endforeach
                     </div>
                 </div>
-                <div class="nav-item me-2">
+                <div class="nav-item">
                     <a
-                        href="{{ route('users.show', [$user, $user->nickname]) }}"
+                        href="{{ route('users.show', [$user, $user->username]) }}"
                         @class([
                             'nav-link', 'lh-1', 'p-2',
                             'bg-azure-lt' => request()->user?->equals($user) && request()->routeIs('users.show')
                         ])
                     >
                         <span class="avatar avatar-sm" style="background-image: url('{{ $user->gravatar() }}');"></span>
-                        <div class="d-none d-sm-block ps-2">
+                        <div class="d-none d-lg-block ps-2">
                             <div class="fw-bold">
                                 {{ $user->nickname }}
                             </div>
@@ -104,21 +93,68 @@
                         </div>
                     </a>
                 </div>
-                <div class="nav-item">
-                    <form action="{{ route('logout') }}" method="post">
-                        @csrf
-                        <button
-                            type="submit"
-                            class="nav-link p-2"
-                            title="Выйти"
-                            data-bs-toggle="tooltip"
-                            data-bs-placement="bottom"
-                        >
-                            <i class="fas fa-sign-out-alt" style="font-size: 30px;"></i>
-                        </button>
-                    </form>
-                </div>
             @endguest
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-menu">
+            {{-- BEGIN NAVBAR MENU --}}
+            <ul class="navbar-nav">
+                {{-- BEGIN GAMES --}}
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#navbar-games" data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button" aria-expanded="false">
+                        <span class="nav-link-icon d-md-none d-lg-inline-block">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-device-gamepad-2">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M12 5h3.5a5 5 0 0 1 0 10h-5.5l-4.015 4.227a2.3 2.3 0 0 1 -3.923 -2.035l1.634 -8.173a5 5 0 0 1 4.904 -4.019h3.4"/>
+                                <path d="M14 15l4.07 4.284a2.3 2.3 0 0 0 3.925 -2.023l-1.6 -8.232"/>
+                                <path d="M8 9v2"/>
+                                <path d="M7 10h2"/>
+                                <path d="M14 10h2"/>
+                            </svg>
+                        </span>
+                        <span class="nav-link-title"> Игры</span>
+                    </a>
+                    <div class="dropdown-menu">
+                        <a href="/games/yandexgames" class="dropdown-item">Яндекс.Игры</a>
+                        <a href="/games/crazygames" class="dropdown-item">CrazyGames</a>
+                        <a href="/games/poki" class="dropdown-item">POKI</a>
+                        <div class="dropdown-divider"></div>
+                        <a href="/games" class="dropdown-item">Витрина</a>
+                    </div>
+                </li>
+                {{-- BEGIN DEVELOPERS --}}
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#navbar-developers" data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button" aria-expanded="false">
+                        <span class="nav-link-icon d-md-none d-lg-inline-block">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-code">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 8l-4 4l4 4"/>
+                                <path d="M17 8l4 4l-4 4"/><path d="M14 4l-4 16"/>
+                            </svg>
+                        </span>
+                        <span class="nav-link-title"> Разработчики</span>
+                    </a>
+                    <div class="dropdown-menu">
+                        <a href="/developers/yandexgames" class="dropdown-item">Яндекс.Игры</a>
+                        <a href="/developers/crazygames" class="dropdown-item">CrazyGames</a>
+                        <a href="/developers/poki" class="dropdown-item">POKI</a>
+                        <div class="dropdown-divider"></div>
+                        <a href="/developers" class="dropdown-item">Витрина</a>
+                    </div>
+                </li>
+                {{-- BEGIN USERS --}}
+                <li class="nav-item">
+                    <a href="{{ route('users') }}" @class(['nav-link', 'bg-azure-lt' => request()->routeIs('users')])>
+                        <span class="nav-link-icon d-md-none d-lg-inline-block">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-users">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 7a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"/>
+                                <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"/><path d="M16 3.13a4 4 0 0 1 0 7.75"/>
+                                <path d="M21 21v-2a4 4 0 0 0 -3 -3.85"/>
+                            </svg>
+                        </span>
+                        <span class="nav-link-title"> Пользователи</span>
+                    </a>
+                </li>
+            </ul>
+            {{-- END NAVBAR MENU --}}
         </div>
     </div>
 </header>
--- resources/views/web/users/profile/index.blade.php
+++ resources/views/web/users/profile/index.blade.php
@@ -3,7 +3,102 @@
 @section('title', "Профиль $user->nickname")
 
 <x-layouts::main>
-    <div class="container">
+    <div class="page-header m-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-auto">
+                    <span class="avatar avatar-2xl bg-azure-lt" style="background-image: url('{{ $user->gravatar(2048) }}')"></span>
+                    <br>
+                    @auth
+                        @if($user->equals(request()->user()))
+                            <form action="{{ route('logout') }}" method="post">
+                                @csrf
+                                <button type="submit" class="btn btn-danger w-100 mt-2" data-loading-text="Выход...">
+                                    <i class="fas fa-sign-out-alt" style="font-size: 16px;"></i>
+                                    <span class="ms-1">Выйти</span>
+                                </button>
+                            </form>
+                        @endif
+                    @endauth
+                </div>
+                <div class="col">
+                    <h1 class="fw-bold m-0">{{ $user->nickname }}</h1>
+                    <h2 class="text-muted">{{ '@' . $user->username }}</h2>
+                    <div class="my-2">Any details such as age, occupation or city. Example: 23 y.o. designer from San Francisco</div>
+                    <div class="list-inline list-inline-dots text-secondary">
+                        <div class="list-inline-item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-inline icon-2">
+                                <path d="M3 7l6 -3l6 3l6 -3v13l-6 3l-6 -3l-6 3v-13"></path>
+                                <path d="M9 4v13"></path>
+                                <path d="M15 7v13"></path>
+                            </svg>
+                            СПб
+                        </div>
+                        <div class="list-inline-item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-inline icon-2">
+                                <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path>
+                                <path d="M3 7l9 6l9 -6"></path>
+                            </svg>
+                            <a href="#" class="text-reset">test@example.com</a>
+                        </div>
+                        <div class="list-inline-item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-inline icon-2">
+                                <path d="M3 20h18v-8a3 3 0 0 0 -3 -3h-12a3 3 0 0 0 -3 3v8z"></path>
+                                <path d="M3 14.803c.312 .135 .654 .204 1 .197a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1c.35 .007 .692 -.062 1 -.197"></path>
+                                <path d="M12 4l1.465 1.638a2 2 0 1 1 -3.015 .099l1.55 -1.737z"></path>
+                            </svg>
+                            15/10/1972
+                        </div>
+                    </div>
+                </div>
+                <div class="col-auto ms-auto">
+                    @auth
+                        @if($user->equals(request()->user()))
+                            <a href="{{ route('users.edit.account', [$user, $user->username]) }}" class="btn btn-primary mb-2 w-100">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-pencil-cog">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                    <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4"/>
+                                    <path d="M13.5 6.5l4 4"/>
+                                    <path d="M17.001 19a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"/>
+                                    <path d="M19.001 15.5v1.5"/>
+                                    <path d="M19.001 21v1.5"/>
+                                    <path d="M22.032 17.25l-1.299 .75"/>
+                                    <path d="M17.27 20l-1.3 .75"/>
+                                    <path d="M15.97 17.25l1.3 .75"/>
+                                    <path d="M20.733 20l1.3 .75"/>
+                                </svg>
+                                Редактировать профиль
+                            </a>
+                        @endif
+                    @endauth
+                    <div class="btn-list">
+                        <a href="#" class="btn btn-2 btn-icon" aria-label="Button">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-2">
+                                <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                                <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                                <path d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                            </svg>
+                        </a>
+                        <a href="#" class="btn btn-2 btn-icon" aria-label="Button" title="Начать диалог">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-2">
+                                <path d="M8 9h8"></path>
+                                <path d="M8 13h6"></path>
+                                <path d="M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z"></path>
+                            </svg>
+                        </a>
+                        <a href="#" class="btn btn-primary btn-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-2">
+                                <path d="M5 12l5 5l10 -10"></path>
+                            </svg>
+                            Following
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container d-none">
         <div class="page-content">
             <div class="row">
                 <div class="col-lg-12">
