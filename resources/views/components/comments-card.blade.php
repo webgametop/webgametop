1efe3d95ef60ba8fc4c55bcfd1ae3914ccd7f246
Noilty <thenoilty@gmail.com>
2026-05-07 22:40:46 +0300

    fixed tmpl

--- resources/views/components/comments-card.blade.php
+++ resources/views/components/comments-card.blade.php
@@ -1,44 +1,51 @@
 @props(['action', 'comments'])
 
 @if(auth()->check())
-    <div class="card bg-azure-lt mb-3">
-        <div class="card-body p-3">
-            <form action="{{ $action }}" method="post">
-                @csrf
-                <textarea
-                    name="comment[body]"
-                    id="comment_body"
-                    cols="30"
-                    rows="5"
-                    placeholder="Оставить комментарий"
-                    class="form-control mb-3"
-                    required
-                ></textarea>
-                <div class="text-end">
-                    <button type="submit" class="btn btn-sm btn-primary" data-loading-text="Отправка...">Отправить</button>
-                </div>
-            </form>
-        </div>
-    </div>
+    <x-ui.subheadline label="Твой комментарий">
+        <form action="{{ $action }}" method="post">
+            @csrf
+            <textarea
+                name="comment[body]"
+                id="comment_body"
+                cols="30"
+                rows="5"
+                class="form-control rounded-0 mb-3"
+                required
+            ></textarea>
+            <div class="text-end">
+                <button type="submit" class="btn btn-sm btn-primary" data-loading-text="Отправка...">Отправить</button>
+            </div>
+        </form>
+    </x-ui.subheadline>
 @endif
 <div class="scrollable">
     <div class="chat">
-        <x-ui.subheadline label="Лента"/>
+        <x-ui.subheadline label="Комментарии"/>
         <div class="chat-bubbles">
             @forelse($comments as $comment)
                 @php($user = $comment->user)
                 <div class="chat-item">
                     <div @class(['row', 'align-items-end', 'justify-content-end flex-row-reverse' => auth()->id() === $user->id])>
                         <div class="col-auto">
-                            <span class="avatar avatar-1" style="background-image: url({{ $user->gravatar() }})"></span>
+                            <a href="{{ route('users.show', [$user, $user->username]) }}" title="Профиль">
+                                <span class="avatar avatar-1" style="background-image: url({{ $user->gravatar() }})"></span>
+                            </a>
                         </div>
                         <div class="col">
                             <div @class(['chat-bubble', 'border', 'bg-azure-lt' => auth()->id() === $user->id])>
                                 <div class="chat-bubble-title">
                                     <div class="row">
-                                        <div class="col chat-bubble-author">{{ $user->nickname }}</div>
+                                        <div class="col chat-bubble-author">
+                                            <span>{{ $user->nickname }}</span>
+                                            <a href="/comments/{{ $comment->id }}">
+                                                <span class="text-muted">{{ '#' . $comment->id }}</span>
+                                            </a>
+                                        </div>
                                         <div class="col-auto chat-bubble-date">
-                                            <span class="text-muted">{{ $comment->created_at->format('Y/m/d H:i') }}</span>
+                                            <span
+                                                class="text-muted"
+                                                title="{{ $comment->created_at->format('Y/m/d H:i:s') }}"
+                                            >{{ $comment->created_at->ago() }}</span>
                                         </div>
                                     </div>
                                 </div>
